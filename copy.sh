bc92bfc356067d2fc51ebaf38a3b3ddb47204259
Alex Miller <apmiller108@yahoo.com>
2022-06-12 20:21:40 -0500

    First commit

---
+++ copy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Copies the mapping files to Mixxx's user controller directory
+cp allen_and_heath_xone_k2* ~/.mixxx/controllers
+
---
+++ cpstart.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Copies the mapping files to Mixxx's user controller directory and starts Mixxx
+# with controller debugging.
+cp allen_and_heath_xone_k2* ~/.mixxx/controllers && pasuspender -- mixxx --controllerDebug
